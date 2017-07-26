904f9f789bcc3740bfc7ff0a2fc18ff2a076217c
vagrant <vagrant@codefin>
2017-07-26 03:05:57 +0000

    Configuração do webpack em gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const elixir = require('laravel-elixir');
-
+const webpackConfig = require('./webpack.config');
+const webpackDevConfig = require('./webpack.dev.config');
 require('laravel-elixir-vue');
+require('laravel-elixir-webpack-official');
 
+Elixir.webpack.config.module.loaders = [];
+
+Elixir.webpack.mergeConfig(webpackConfig);
+Elixir.webpack.mergeConfig(webpackDevConfig);
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
